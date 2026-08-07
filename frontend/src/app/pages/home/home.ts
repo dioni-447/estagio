1c8619772cbfe44446f187a4d7b2c0c32c024bf7
João Vitor <joaovitor.santos.goulart@gmail.com>
2026-08-07 10:42:56 -0300

    atualização projeto

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {Hero} from '../../components/hero/hero';
 import {Servicos} from '../../components/servicos/servicos';
 import {Contato} from '../../components/contato/contato';
+import { Sobre } from '../../components/sobre/sobre';
 
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, Servicos, Contato],
+  imports: [Hero, Servicos, Contato,Sobre],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
